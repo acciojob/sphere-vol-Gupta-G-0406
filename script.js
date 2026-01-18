e3830129338c3de100d7466f1de46b6e4d016f24
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 15:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  let radius = parseFloat(document.getElementById("radius").value);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  let volume;
+  if (isNaN(radius) || radius < 0) {
+    volume = NaN;
+  } else {
+    volume = ((4 / 3) * Math.PI * Math.pow(radius, 3)).toFixed(4);
+  }
+
+  document.getElementById("volume").value = volume;
+  return false;
+}
+
+window.onload = function () {
+  document.getElementById("MyForm").onsubmit = volume_sphere;
+};
